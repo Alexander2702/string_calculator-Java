21b88eae7db569cb09395a56bc609f2d2c9b7417
Alexander2702 <anisimov_am@mail.ru>
2021-12-20 16:11:10 +0400

    string_calculator-Java
    string_calculator-Java

--- src/MinusStr.java
+++ src/MinusStr.java
@@ -1,12 +1,6 @@
 public class MinusStr {
     public static String minus(String str1, String str2){
 
-        boolean cb = str1.contains(str2);
-        if (cb) {
-            int a = str1.length() - str2.length();
-            return str1.substring(0, a);
-        } else {
-            return str1;
-        }
+        return str1.replace(str2,"");
     }
 }
